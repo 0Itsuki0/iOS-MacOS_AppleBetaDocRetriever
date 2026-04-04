fbbe75c275b4d5b60fd85156c5217fd27276ca11
itsuki <itsuki.enjyoy@gmail.com>
2026-04-04 13:55:07 +0900

    Initial Commit

---
+++ AppleBetaDocRetriever/AppleBetaDocRetrieverApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleBetaDocRetrieverApp.swift
+//  AppleBetaDocRetriever
+//
+//  Created by Itsuki on 2026/04/04.
+//
+
+import SwiftUI
+
+@main
+struct AppleBetaDocRetrieverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
